f3936c63b4e1a63cf2f0169ba8ebfd2d8a0377c0
xvw <xaviervdw@gmail.com>
2019-01-17 14:11:18 +0100

    Rename uuid module

---
+++ src/bedrock/uuid.mli
@@ -0,0 +1,24 @@
+(** Module to build Unique identifiers *)
+
+type t = string
+
+(** {2 Requirement} *)
+
+module type GENERATOR = sig
+  val name : unit -> string
+  val pid : unit -> int
+  val time : unit -> float
+end
+
+(** {2 Global API} *)
+
+(** Create and [UUID]. *)
+val make : (module GENERATOR) -> unit -> t
+
+(** {2 Functor} *)
+
+module Generator (G : GENERATOR) : sig
+  type t = string
+
+  val make : unit -> t
+end

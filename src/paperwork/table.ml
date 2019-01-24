1c866d0b60823f14c3f7a8ddf572342dc170c9da
xvw <xaviervdw@gmail.com>
2019-01-24 03:23:54 +0100

    Improve Table API

--- src/paperwork/table.ml
+++ src/paperwork/table.ml
@@ -2,8 +2,9 @@ open Bedrock
 open Error
 
 type ('a, 'b) t = ('a, 'b) Hashtbl.t
+type configuration = (string, Qexp.t option) t
 
-let from_qexp f qexp =
+let from_qexp_gen f qexp =
   match qexp with
   | Qexp.Node elts ->
     let table = Hashtbl.create 1 in
@@ -27,4 +28,29 @@ let from_qexp f qexp =
     aux (Ok table, elts)
   | _ ->
     Error (No_root_element (Qexp.to_string qexp))
+;;
+
+let from_qexp f =
+  let rf x l =
+    let open Qexp in
+    match x with
+    | Atom key | Tag key | Keyword key | String (_, key) ->
+      f key l
+    | q ->
+      Error (Not_a_valid_node (to_string q))
+  in
+  from_qexp_gen rf
+;;
+
+let configuration =
+  let open Qexp in
+  let f key = function
+    | [x] ->
+      Ok (key, Some x)
+    | [] ->
+      Ok (key, None)
+    | list ->
+      Ok (key, Some (node list))
+  in
+  from_qexp f
 ;;
--- src/paperwork/table.mli
+++ src/paperwork/table.mli
@@ -5,11 +5,21 @@ open Bedrock
 (** {2 Types} *)
 
 type ('a, 'b) t = ('a, 'b) Hashtbl.t
+type configuration = (string, Qexp.t option) t
 
 (**{2 Builder} *)
 
 (** Build a [Table.t] from a [Qexp.t] *)
-val from_qexp :
+val from_qexp_gen :
      (Qexp.t -> Qexp.t list -> ('a * 'b) Result.t)
   -> Qexp.t
   -> ('a, 'b) t Result.t
+
+(** Build a [Table.t] from a [Qexp.t] with k/v [Qexp.t]. *)
+val from_qexp :
+     (string -> Qexp.t list -> ('a * 'b) Result.t)
+  -> Qexp.t
+  -> ('a, 'b) t Result.t
+
+(** Build a [Table.configuration] from a [Qexp.t]. *)
+val configuration : Qexp.t -> configuration Result.t

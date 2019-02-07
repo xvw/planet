0859c948db0ae28db7546e747accba4f137b2886
xvw <xaviervdw@gmail.com>
2019-02-08 00:21:06 +0100

    Add "show sectors" for log.exe

--- src/bin/log/cli.ml
+++ src/bin/log/cli.ml
@@ -1,1 +1,22 @@
-let invoke () = ()
+let call = "./log.exe"
+let version = "1.0.0"
+
+open Cmdliner
+
+let sectors =
+  let doc = "Show the list of available sectors" in
+  let man = Glue.Man.default call in
+  let exits = Term.default_exits in
+  ( Term.(const Lib.sectors $ const ())
+  , Term.info "sectors" ~version ~doc ~exits ~man )
+;;
+
+let index =
+  let doc = "Manage logs for timetracking" in
+  let man = Glue.Man.default call in
+  let exits = Term.default_exits in
+  ( Term.(ret (const (`Help (`Pager, None))))
+  , Term.info call ~version ~doc ~exits ~man )
+;;
+
+let invoke () = Term.(exit @@ eval_choice index [sectors])
--- src/bin/log/lib.ml
+++ src/bin/log/lib.ml
@@ -1,1 +1,21 @@
-let sectors () = ()
+open Paperwork
+open Baremetal
+
+let sectors () =
+  match Glue.Sector.all () with
+  | Ok hashtable ->
+    let () =
+      Ansi.[bold; fg cyan; !"Available sectors\n"]
+      |> Ansi.to_string |> print_endline
+    in
+    Hashtbl.iter
+      (fun _ sector ->
+        let open Shapes.Sector in
+        let open Ansi in
+        text_box sector.name sector.desc
+        @ [reset; fg cyan; !(Color.to_hex sector.color); reset; !"\n"]
+        |> to_string |> print_endline )
+      hashtable
+  | Error errs ->
+    Glue.Ui.prompt_errors errs
+;;

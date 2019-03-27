d6df8a8cc20db87fc8758932584f597c770118d1
xvw <xaviervdw@gmail.com>
2019-03-27 17:20:30 +0100

    Small correction on dumping logs

--- src/bin/log/lib.ml
+++ src/bin/log/lib.ml
@@ -159,8 +159,8 @@ let push_result log =
         in
         Ok filename )
   >>= fun filename ->
-  let str_log = log |> qexpify |> Qexp.to_string in
-  File.append filename ("\n" ^ str_log) >> Ok (filename, str_log)
+  let str_log = log |> qexpify |> Qexp.to_string |> String.trim in
+  File.append filename ("\n\n" ^ str_log) >> Ok (filename, str_log)
 ;;
 
 let ensure_sectors_projects f =

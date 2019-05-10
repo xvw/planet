ced28808f323c328be492287f0d8d469b965670f
xvw <xaviervdw@gmail.com>
2019-05-10 05:15:53 +0200

    Remove templating stuff

--- test/paperwork_test/paperwork_test.ml
+++ test/paperwork_test/paperwork_test.ml
@@ -1,6 +1,6 @@
 let suite =
   [ "Paperwork.Qexp", Qexp_test.suite
   ; "Paperwork.Timetable", Timetable_test.suite
-  ; "Paperwork.Html", Html_test.suite
-  ; "Paperwork.Color", Color_test.suite ]
+  ; "Paperwork.Color", Color_test.suite
+  ]
 ;;

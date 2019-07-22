0f6080505d18905a2fdef69ba413222d29f1d907
xvw <xaviervdw@gmail.com>
2019-07-22 02:28:27 +0200

    Update Site.hs for project Generation

--- site.hs
+++ site.hs
@@ -1,41 +1,73 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
 
-
---------------------------------------------------------------------------------
-unseedRoute = gsubRoute "_seeds/" (const "")
-
+-- Main process
 main :: IO ()
 main = hakyll $ do
 
+    -- Templates compilation
     match "templates/*" $ compile templateBodyCompiler
-    
+
+    -- Static images
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
 
+    -- Static pictograms
     match ("picto/*.png" .||. "picto/*.svg") $ do
-        route   idRoute
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
 
+    -- Css for styles and fonts
     match ("css/*.css" .||. "fonts/*/*.css") $ do
-        route   idRoute
-        compile compressCssCompiler
+      route   idRoute
+      compile compressCssCompiler
 
+    -- Custom fonts
     match ("fonts/*/*.woff" .||. "fonts/*/*.woff2") $ do
-        route   idRoute
-        compile copyFileCompiler
- 
-    match "_seeds/api/*.json" $ do
-        route (unseedRoute `composeRoutes` setExtension "json")
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
 
+    -- Seeded JSON files (from Planet)
+    match "_seeds/api/*.json" $ do
+      route (unseedRoute `composeRoutes` setExtension "json")
+      compile copyFileCompiler
+
+    -- Project seeded (from Planet)
+    match ("_seeds/projects/*.org"
+            .||. "_seeds/projects/*.md"
+            .||. "_seeds/projects/*.markdown"
+            .||. "_seeds/projects/*.rst"
+          ) $ do
+      route $ setExtension "html"
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" projectContext
+        >>= relativizeUrls
+      
+    -- Index.html
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            getResourceBody
-                >>= applyAsTemplate defaultContext
-                >>= loadAndApplyTemplate "templates/default.html" defaultContext
-                >>= relativizeUrls
+      route idRoute
+      compile $ do
+        getResourceBody
+          >>= applyAsTemplate defaultContext
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
+
+
+
+-- Remove artifacts in routes
+unseedRoute = gsubRoute "_seeds/" (const "")
+
+-- Custom Contexts
+
+projectContext :: Context String
+projectContext =
+  bodyField      "body"              `mappend`
+  titleField     "title"             `mappend`
+  titleField     "name"              `mappend`
+  teaserField    "teaser" "content"  `mappend`
+  urlField       "url"               `mappend`
+  pathField      "path"              `mappend`
+  metadataField                      `mappend`
+  missingField

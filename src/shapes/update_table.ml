fe23a11e475b2ccd43a9fd32ce26c3910663c72b
xvw <xaviervdw@gmail.com>
2019-07-24 23:03:38 +0200

    Append Update coercion function

--- src/shapes/update_table.ml
+++ src/shapes/update_table.ml
@@ -1,1 +1,45 @@
-type t = (string, Paperwork.Timetable.Day.t) Hashtbl.t
+open Bedrock
+open Paperwork
+open Util
+open Error
+
+type t = (string, Timetable.Day.t) Hashtbl.t
+
+let store_update table = function
+  | Qexp.(Node [ String (_, name); Keyword timecode ]) ->
+    let open Result in
+    let* code = Timetable.Day.from_string timecode in
+    let+ final_table =
+      let () = Hashtbl.remove table name in
+      let () = Hashtbl.add table name code in
+      return table
+    in
+    final_table
+  | qexp ->
+    Error (Not_a_valid_node (Qexp.to_string qexp))
+;;
+
+let from_qexp = function
+  | Qexp.Node elts ->
+    let table = Hashtbl.create 1 in
+    List.fold_left
+      (fun potential_table expr ->
+        let open Result.Syntax in
+        let* hashtable = potential_table in
+        store_update hashtable expr)
+      (Ok table)
+      elts
+  | qexp ->
+    Error (No_root_element (Qexp.to_string qexp))
+;;
+
+let to_qexp table =
+  Hashtbl.fold
+    (fun key value acc ->
+      let open Qexp in
+      node [ string key; keyword $ Timetable.Day.to_string value ]
+      :: acc)
+    table
+    []
+  |> Qexp.node
+;;
--- src/shapes/update_table.mli
+++ src/shapes/update_table.mli
@@ -1,5 +1,13 @@
 (** Store updates into a flat key-value engine *)
 
+open Bedrock
+open Paperwork
+
 (** {2 Types} *)
 
 type t
+
+(** {2 Coercion} *)
+
+val from_qexp : Qexp.t -> t Result.t
+val to_qexp : t -> Qexp.t

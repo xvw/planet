4762f1664c25aec6a4d5b134af4b091836731f31
xvw <xaviervdw@gmail.com>
2019-08-18 03:02:45 +0200

    Handle line number in code block

--- src/facade/roe.ml
+++ src/facade/roe.ml
@@ -1,10 +1,27 @@
 open Js_of_ocaml
 open Bedrock
+open Util
 open Error
 module Tyxml = Js_of_ocaml_tyxml.Tyxml_js
 
 module Code = struct
-  let underbox parent node =
+  let prepare parent node =
+    let () = Dom.removeChild parent node in
+    let open Tyxml.Html in
+    let subparent =
+      div
+        ~a:[ a_class [ "code-container" ] ]
+        [ div
+            ~a:[ a_class [ "code-concrete-container" ] ]
+            [ Tyxml.Of_dom.of_div node ]
+        ]
+      |> Tyxml.To_dom.of_div
+    in
+    let () = Dom.appendChild parent subparent in
+    Ok (subparent, node)
+  ;;
+
+  let underbox parent (subparent, node) =
     if Attr.Data.(parent.?{"pellet"} || parent.?{"file"})
     then
       let open Tyxml.Html in
@@ -23,18 +40,48 @@ module Code = struct
       let box =
         div ~a:[ a_class [ "code-underbox" ] ] (pellet @ file)
       in
-      let () = Dom.appendChild node (Tyxml.To_dom.of_div box) in
-      Ok node
-    else Ok node
+      let () = Dom.appendChild subparent (Tyxml.To_dom.of_div box) in
+      Ok (subparent, node)
+    else Ok (subparent, node)
   ;;
 
-  let line_number parent node = Ok node
+  let line_number parent (subparent, node) =
+    if Attr.Data.(parent.?{"line-number"})
+    then (
+      let l =
+        (parent##querySelectorAll (Js.string ".sourceLine"))##.length
+      in
+      let open Tyxml.Html in
+      let offset =
+        Attr.Data.(parent.%{"line-start"})
+        |> Option.bind int_of_string_opt
+        |> Option.get_or (fun () -> 1)
+      in
+      let box =
+        div
+          ~a:[ a_class [ "code-line-number" ] ]
+          (List.init l (fun i ->
+               div [ txt $ Format.asprintf "%d" (offset + i) ]))
+        |> Tyxml.To_dom.of_div
+      in
+      let receiver =
+        subparent##querySelector
+          (Js.string "div.code-concrete-container")
+        |> Js.Opt.to_option
+        |> Option.get_or (fun () -> subparent)
+      in
+      let () = Dom.appendChild receiver box in
+      Ok (subparent, node))
+    else Ok (subparent, node)
+  ;;
 
   let deal_with parent nodes =
     let open Validation.Infix in
     let list = Dom.list_of_nodeList nodes in
     List.map
-      (fun node -> node |> line_number parent >>= underbox parent)
+      (fun node ->
+        node |> prepare parent >>= line_number parent
+        >>= underbox parent)
       list
     |> Validation.Applicative.sequence >> Ok ()
   ;;

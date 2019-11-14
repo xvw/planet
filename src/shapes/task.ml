3eff0055207e66c0b42e4e8071f3cae9677f632b
xvw <xaviervdw@gmail.com>
2019-11-14 18:22:36 +0100

    Update OCaml format and initialize Task

--- src/shapes/task.ml
+++ src/shapes/task.ml
@@ -8,12 +8,13 @@ type t =
   { state : state
   ; uuid : string
   ; project : string option
-  ; main_sector : string
-  ; priority : int
+  ; sectors : string list
   ; name : string
   ; description : string
+  ; checklist : (bool * string) list
   ; tags : string list
   ; date : Paperwork.Timetable.Day.t
   ; opening_date : Paperwork.Timetable.Day.t option
   ; closing_date : Paperwork.Timetable.Day.t option
+  ; engagement_date : Paperwork.Timetable.Day.t option
   }

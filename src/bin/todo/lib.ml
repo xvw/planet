f7a72649fcf29181c5e1bd886ef7fd5412ee099d
xvw <xaviervdw@gmail.com>
2019-12-28 03:56:45 +0100

    Remove temp files

--- src/bin/todo/lib.ml
+++ src/bin/todo/lib.ml
@@ -4,5 +4,6 @@ let create () =
   Binutil.ensure_sectors_projects (fun _sectors (_ctx, projects) ->
       let _some_project =
         Binutil.may_project (List.map (fun (x, _, _) -> x) projects) in
+      let _sectors = () in
       ())
 ;;
